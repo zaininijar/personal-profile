f26147a9c27eacea36e50870e2534251d427be1a
zaininijar <zaininijar7610@gmail.com>
2024-06-14 05:49:18 +0700

    initial repo + little custom

---
+++ app/page.tsx
@@ -0,0 +1,58 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 min-h-full">
+      <div className="inline-block max-w-lg text-center justify-center">
+        <h1 className={title()}>Ahmad&nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>Zaini&nbsp;</h1>
+        <h1 className={title()}>Nijar&nbsp;</h1>
+        <br />
+        <h1 className={title({ size: "xs" })}>Software Engineer</h1>
+        <h2 className={subtitle({ class: "mt-4" })}>
+          Laravel, Next, Express, Php, Go, Typescript
+        </h2>
+      </div>
+
+      <div className="flex gap-3">
+        <Link
+          isExternal
+          className={buttonStyles({
+            color: "primary",
+            radius: "full",
+            variant: "shadow",
+          })}
+          href={siteConfig.links.getCv}
+        >
+          Download CV
+        </Link>
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+      </div>
+
+      <div className="mt-8">
+        <Snippet hideCopyButton hideSymbol variant="flat">
+          <div>
+            Explore my personal profile as a
+            <Code color="primary" className="ml-2">
+              Software Engineer
+            </Code>
+          </div>
+        </Snippet>
+      </div>
+    </section>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,69 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Ahmad Zaini Nijar",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/zaininijar",
+    twitter: "https://twitter.com/zaininijar",
+    discord: "https://discord.gg/zaininijar",
+    sponsor: "https://patreon.com/zaininijar",
+    getCv: "https://drive.google.com/file/d/1iifU2dR4X6AIFExfg5BQos-OC0lTvVa7/view"
+  },
+};
